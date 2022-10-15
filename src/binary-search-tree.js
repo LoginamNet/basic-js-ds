51047f24d33ccdd70f7e3c7c6d47d5a122b2b88b
LoginamNet <loginamnet@gmail.com>
2022-10-15 13:41:59 +0300

    feat: add add class

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -16,8 +16,21 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    function addData(node, data) {
+      if (!node) return new Node(data);
+      if (node.data === data) return node;
+      
+      if (node.data > data) {
+        node.left = addData(node.left, data)
+      } else if (node.data < data) {
+        node.right = addData(node.right, data)
+      }
+
+      return node;
+    }
+
+    this.tree = addData(this.tree, data);
     // remove line with error and write your code here
   }
 
